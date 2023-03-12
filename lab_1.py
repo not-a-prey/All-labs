99aa9e6f1cef3d499eff862ee7cdcebf6dc8522c
not_a_prey <119671453+not-a-prey@users.noreply.github.com>
2023-03-12 22:33:07 +0200

    Add files via upload

---
+++ lab_1.py
@@ -0,0 +1,18 @@
+a = input("Enter a: ")
+b = input("Enter b: ")
+n = input("Enter n: ")
+m = input("Enter m: ")
+sum1 = 0.0
+
+try:
+    new_n = int(n)
+    new_m = int(m)
+    for i in range(int(a), new_n+1):
+        for j in range(int(b), new_m+1):
+            sum1 += (i/j)/(i+2)
+    print("Answer is {0}".format(sum1))
+except ZeroDivisionError:
+    print("Невизначеність: ділення на нуль")
+except (TypeError, ValueError):
+    print("Ви ввели не цілі числа")
+
